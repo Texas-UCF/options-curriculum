7be007d20c07398409a7e3c7f48c3f3b16d89bcb
Neil Vyas <vyas.neil.nv@gmail.com>
2015-04-20 18:49:42 -0500

    first commit.

---
+++ build.py
@@ -0,0 +1,42 @@
+from staticjinja import make_site
+from markdown import markdown
+from collections import OrderedDict as od
+def get_sections():
+  sections = od([
+    ('Basic Terminology', od(
+        [
+          ('Calls and Puts', 'calls_and_puts.html'),
+          ('Optionality', 'optionality.html'),
+          ('Contracts', 'contract_specifications.html'),
+          ('Exercising', 'when_to_exercise.html'),
+          ('Moneyness', 'moneyness.html'),
+          ('Downside vs Upside', 'down_upside.html'),
+          ('Cost of Options', 'options_cost.html'),
+        ]
+      )
+    ),
+    ('Profit Diagrams', od(
+        [
+          ('Payoff', 'payoff_diagram.html'),
+          ('Profit at Expiration', 'profit_at_expiration.html'),
+        ]
+      )
+    )
+    ]
+  )
+  return {'sections': sections}
+
+def get_contents(template):
+  with open(template.filename) as f:
+    return {'page': f.read()}
+
+if __name__ == '__main__':
+  site = make_site(
+      contexts=[
+        ('.*.html', get_sections),
+      ],
+      outpath="site",
+      staticpaths=['static'],
+    )
+
+  site.render(use_reloader=True)
